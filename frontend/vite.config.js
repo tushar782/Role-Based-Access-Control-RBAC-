05f636ac52301abe0b3389184cda1dc3848cf940
Tushar Pimple <99522469+tushar782@users.noreply.github.com>
2025-02-11 13:48:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
 })
